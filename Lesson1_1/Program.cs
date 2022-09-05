ba14e1937d390fd5bce2211c10bc530f841143b1
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-09-05 18:03:45 +0300

    Add project files.

---
+++ Lesson1_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson1_1
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is line
+            /*this is comment
+             */
+
+           // //פלט
+           // Console.WriteLine("hello world");
+           // //קלט
+           //// Console.ReadLine();
+
+            //variable
+            string firstName = "";
+            string name = "yarin";
+            int age = 30;
+            //ctr +f5
+            Console.WriteLine("hello "+name +"your age :"+age);
+            Console.WriteLine("hello {0} your age {1}",name,age);
+            Console.ReadLine();
+
+
+
+
+            
+        }
+    }
+}
